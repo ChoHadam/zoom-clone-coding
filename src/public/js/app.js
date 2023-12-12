9d1c0c49ec267248251b3234ad3509a5d5a96291
조하담 <35447878+ChoHadam@users.noreply.github.com>
2023-12-12 02:07:44 +0000

    Create websocket connection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,4 @@
-alert("hi");
+// https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/WebSocket
+
+// const socket = new WebSocket("ws://localhost:3000");
+const socket = new WebSocket(`ws://${window.location.host}`); // 여기서의 socket은, 서버로의 연결을 뜻한다.
