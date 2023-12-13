973a4002babe7131b08273a5c2720487a6a2ba04
조하담 <35447878+ChoHadam@users.noreply.github.com>
2023-12-13 03:04:50 +0000

    Add nickname input form to identify sockets by nickname

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,30 +1,53 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nicknameForm = document.querySelector("#nickname");
+const chatForm = document.querySelector("#chat");
 
 const socket = new WebSocket(`ws://${window.location.host}`); // 여기서의 socket은, 서버로의 연결을 뜻한다.
 
+socket.addEventListener("open", handleOpen);
+socket.addEventListener("message", (message)=> {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
+socket.addEventListener("close", handleClose);
+
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
+chatForm.addEventListener("submit", handleChatSubmit);
+
+
 function handleOpen() {
     console.log("Connected to Server ✔️");
-}
-
-function handleMessage(message) {
-    console.log("New message: ", message.data)
 }
 
 function handleClose() {
     console.log("Disconnected from Server ❌");
 }
 
-socket.addEventListener("open", handleOpen);
-socket.addEventListener("message", handleMessage);
-socket.addEventListener("close", handleClose);
-
-
-function handleSubmit(event) {
+function handleNicknameSubmit(event) {
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    const input = nicknameForm.querySelector("input");
+    socket.send( // 왜 string data만 취급할까? 어떤 언어를 사용하는 서버가 받을지 모르는데, 특정 언어에 의존한 객체를 보내는건 좋지 않아. => 즉, 이 websocket은 "브라우저"에 있는 API이기 때문이다.
+        JSON.stringify(
+            {
+                type: "nickname",
+                payload: input.value
+            }
+        )
+    );
     input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSubmit);
+function handleChatSubmit(event) {
+    event.preventDefault();
+    const input = chatForm.querySelector("input");
+    socket.send(
+        JSON.stringify(
+            {
+                type: "chat",
+                payload: input.value
+            }
+        )
+    );
+    input.value = "";
+}
--- src/server.js
+++ src/server.js
@@ -13,23 +13,39 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app); //서버에 접근할 수 있게 됨 = 이 서버에 웹소켓 만들 수 있게 됨
-const wss = new WebSocket.Server({server}); // http 서버 우에 ws 서버를 올린다. 이렇게 인자로 server 넘기면 같은 서버에서 http와 ws 둘 다 돌릴 수 있게 된다. 두 개가 동일한 포트에 있길 바랄때 이렇게 쓴다.
-
-function onSocketClose() {
-    console.log("Disconnected from Browser ❌")
-}
+const wss = new WebSocket.Server({ server }); // http 서버 우에 ws 서버를 올린다. 이렇게 인자로 server 넘기면 같은 서버에서 http와 ws 둘 다 돌릴 수 있게 된다. 두 개가 동일한 포트에 있길 바랄때 이렇게 쓴다.
 
 const sockets = []; // 연결된 소켓들을 저장하는 메모리 저장소.
-
-function handleConnection(socket) { //여기서 소켓은 서버와 연결된 브라우저이다.
-    sockets.push(socket);
-    console.log("Connected to Browser ✔️");
-    socket.on("close", onSocketClose);
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message));
-    });
-}
 
 wss.on("connection", handleConnection); // on 메소드가 브라우제에서 벡엔드로 연결된 사람의 정보를 제공해주는데, 소켓에 담겨온다.
 
 server.listen(3000, handleListen);
+
+function onSocketClose() {
+  console.log("Disconnected from Browser ❌");
+}
+
+function handleConnection(socket) {
+  //여기서 소켓은 서버와 연결된 브라우저이다.
+  console.log("Connected to Browser ✔️");
+
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+
+  socket.on("close", onSocketClose);
+
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+
+    switch (message.type) {
+      case "chat":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname} : ${message.payload}`),
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break; // case 마다 break로 끊어줘야 닉네임이 의도치않게 바뀌지 않는다. case문은 break를 만날때까지 동작하기 때문이다.
+    }
+  });
+}
