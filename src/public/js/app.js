84abd5e6f0f27ab5e99dc9ebb3df2cf2a39aa2d0
조하담 <hadamcho97@gmail.com>
2023-12-21 17:35:04 +0900

    Chat through chat messages in chat room and notify socket disconnection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,34 +1,52 @@
+const socket = io(); //백엔드의 socket.io를 프론트와 연결한다. io 메서드가 알아서 socket.io를 실행하고 있는 백엔드 서버를 찾는다. (설치는 view파일에서 했다.)
+
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
 const room = document.getElementById("room");
+const welcomeForm = welcome.querySelector("form");
+const roomForm = room.querySelector("form");
+
 room.hidden = true;
 
 let roomName;
 
-const socket = io(); //백엔드의 socket.io를 프론트와 연결한다. io 메서드가 알아서 socket.io를 실행하고 있는 백엔드 서버를 찾는다. (설치는 view파일에서 했다.)
-
-form.addEventListener("submit", handleRoomSubmit);
+welcomeForm.addEventListener("submit", hadleWelcomeFormSubmit);
 
 socket.on("welcome", () => addMessage("someone joined!"));
 
-function handleRoomSubmit(event) {
-    event.preventDefault();
-    const input = form.querySelector("input");
-    socket.emit("enter_room", input.value, showRoom); // 세번째 파라미터는 callback이다. 서버가 이 callback을 받아서 호출하면, 브라워에서 함수가 실행되는 매직!
-    roomName = input.value;
-    input.value = "";
+socket.on("new_message", (msg) => addMessage(msg));
+
+socket.on("bye", () => addMessage("someone disconnected!"));
+
+function hadleWelcomeFormSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  const content = input.value;
+  socket.emit("enter_room", content, showRoom); // emit() 파라미터 중에서 함수가 세번째 파라미터로 넘어갔네. 이건 callback이다. 서버가 이 callback을 받아서 호출하면, 브라워에서 함수가 실행되는 매직!
+  roomName = content;
+  input.value = "";
 }
 
 function showRoom() {
-    welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName}`;
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  roomForm.addEventListener("submit", handleMessageSubmit);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const content = input.value;
+  socket.emit("new_message", content, roomName, () => {
+    addMessage(`You: ${content}`);
+  });
+  input.value = "";
 }
 
 function addMessage(msg) {
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerText = msg;
-    ul.appendChild(li);
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = msg;
+  ul.appendChild(li);
 }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import http from "http";
-// socket.io는 websocket의 구현체가 아니다. 
+// socket.io는 websocket의 구현체가 아니다.
 // 웹소켓을 일부 사용하면서 여러 기능을 제공하는 새로운 프레임워크다. (채팅방, 재연결 등)
 // 따라서, 만약 websocket 이용이 불가능해져도, socket.io는 다른 방법을 이용해서 계속해서 동작한다. (HTTP long-polling)
-import SocketIO from "socket.io"; 
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -17,15 +17,21 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  
   socket.on("enter_room", (roomName, done) => {
-    // console.log('socket.rooms', socket.rooms); // Set {<socket.id>} (현재 속해있는 room들을 보여준다. 이미 자신의 socket id 이름의 방에 혼자 들어가있다.)
     socket.join(roomName);
-    // console.log('socket.rooms', socket.rooms); // Set {<socket.id>, <roomName>}
     done();
     socket.to(roomName).emit("welcome");
-  }); 
+  });
 
+  socket.on("new_message", (message, roomName) => {
+    socket.to(roomName).emit("new_message", message);
+  });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => {
+      socket.to(room).emit("bye");
+    });
+  }); // 소켓 접속이 끊어지는것을 감지한다.
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
